656e12d27ab7eff22fbe3b794fbd59399e6cc9cd
Akmaral <makosya1234567@gmail.com>
2020-11-11 15:40:32 +0600

    Initial commit

---
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text1;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text1 = (TextView) findViewById(R.id.text1);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btn1:
+                        text1.setText(R.string.text1);
+                        break;
+                    case R.id.btn2:
+                        text1.setText(R.string.text2);
+                        break;
+                    case R.id.btn3:
+                        text1.setText(R.string.text3);
+                        break;
+                }
+            }
+        };
+
+        btn1.setOnClickListener(onClickListener);
+        btn2.setOnClickListener(onClickListener);
+        btn3.setOnClickListener(onClickListener);
+    }
+}
